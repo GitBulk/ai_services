e2f72d8cb45235ac81499568e6d4f016ec78d310
GitBulk <giacatiensinh@hotmail.com>
2026-03-25 14:37:20 +0700

    [f] update service registry

--- app/core/service_registry.py
+++ app/core/service_registry.py
@@ -1,40 +1,50 @@
 from app.core.settings import settings
 from app.services.faiss_vector_service import FaissVectorService
+from app.services.in_memory_vector_service import InMemoryVectorService
 from app.services.ranking.cross_encoder_reranker import CrossEncoderReranker
 from app.services.ranking.heuristic_reranker import HeuristicReranker
 from app.services.retrieval.retriever import Retriever
 from app.services.search_service import SearchService
 from app.services.text_embedding_service import TextEmbeddingService
-from app.services.in_memory_vector_service import InMemoryVectorService
+
 
 class ServiceRegistry:
     def __init__(self, settings):
-        self.text_service = None
         self.settings = settings
         self.services = {}
 
     def initialize(self, model_registry, resource_manager):
         # text service
-        text_model = model_registry.get('text_embedding')
-        self.services['text'] = TextEmbeddingService(text_model)
+        text_model = model_registry.get("text_embedding")
+        self.services["text"] = TextEmbeddingService(text_model)
 
         # vector service
-        if self.settings.VECTOR_BACKEND == 'faiss':
-            self.services['text_vector'] = FaissVectorService(resource_manager=resource_manager, index_name='text', use_cosine = True)
-            self.services['product_vector'] = FaissVectorService(resource_manager=resource_manager, index_name='product', use_cosine = True)
+        if self.settings.VECTOR_BACKEND == "faiss":
+            self.services["text_service"] = FaissVectorService(resource_manager=resource_manager, index_name="text")
+            self.services["product_service"] = FaissVectorService(
+                resource_manager=resource_manager, index_name="product"
+            )
         else:
-            self.services['text_vector'] = InMemoryVectorService(self.text_service)
+            self.services["text_service"] = InMemoryVectorService(self.services["text"])
 
-        if self.settings.RERANKER == 'cross':
-            self.services['reranker'] = CrossEncoderReranker()
+        if self.settings.RERANKER == "cross":
+            self.services["reranker"] = CrossEncoderReranker()
         else:
-            self.services['reranker'] = HeuristicReranker()
+            self.services["reranker"] = HeuristicReranker()
 
-        self.services['retriever'] = Retriever(vector_service=self.services['text_vector'], text_service=self.services['text'])
-        self.services['search'] = SearchService(retriever=self.services['retriever'], reranker=self.services['reranker'])
+        self.services["retriever"] = Retriever(
+            vector_service=self.services["text_service"], text_service=self.services["text"]
+        )
+        self.services["search"] = SearchService(
+            retriever=self.services["retriever"], reranker=self.services["reranker"]
+        )
+
+        # scam detection service
+        # self.services["scam_detection"] = ScamDetectionService()
 
     def get(self, name):
         return self.services[name]
+
 
 # ServiceRegistry → quản lý service
 service_registry = ServiceRegistry(settings)
